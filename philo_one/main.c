f7d71082b059c5ac5107e3fc65973a17ff6878bb
melon <jack716@naver.com>
2021-03-18 23:52:19 +0900

    test

--- philo_one/main.c
+++ philo_one/main.c
@@ -6,7 +6,7 @@
 /*   By: hroh <hroh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 22:17:59 by hroh              #+#    #+#             */
-/*   Updated: 2021/03/18 23:50:03 by hroh             ###   ########.fr       */
+/*   Updated: 2021/03/18 23:52:00 by hroh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,11 @@ int		ft_make_thread(t_env *env)
 		if (pthread_create(&(env->p[i]->th_id), NULL, ft_routine, (void *)env->p[i]) != 0)
 			return (ft_putstr("Error : pthread_create error\n"));
 		pthread_detach(env->p[i]->th_id);
+		usleep(100);
+	}
+	i = -1;
+	while (++i < env->n_philo)
+	{
 		if (pthread_create(&th_id, NULL, &ft_dead_monitor, (void *)env->p[i]) != 0)
 			return (ft_putstr("Error : pthread_create error\n"));
 		pthread_detach(th_id);
