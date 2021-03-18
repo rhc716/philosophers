c4d3db342c661e99049d03564b419ef75ed002b7
melon <jack716@naver.com>
2021-03-18 18:06:17 +0900

    fork

--- philo_one/action.c
+++ philo_one/action.c
@@ -6,7 +6,7 @@
 /*   By: hroh <hroh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 19:22:42 by hroh              #+#    #+#             */
-/*   Updated: 2021/03/18 17:51:00 by hroh             ###   ########.fr       */
+/*   Updated: 2021/03/18 18:05:36 by hroh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,25 @@ void		ft_take_fork(t_philo *philo)
 	if ((philo->index + 1) > (philo->env->n_philo / 2))
 	{
 		pthread_mutex_lock(&philo->env->forks[philo->l_fork]);
+		pthread_mutex_lock(&philo->env->print);
+		ft_put_msg(philo, EVENT_FORK);
+		pthread_mutex_unlock(&philo->env->print);
 		pthread_mutex_lock(&philo->env->forks[philo->r_fork]);
+		pthread_mutex_lock(&philo->env->print);
+		ft_put_msg(philo, EVENT_FORK);
+		pthread_mutex_unlock(&philo->env->print);
 	}
 	else
 	{
 		pthread_mutex_lock(&philo->env->forks[philo->r_fork]);
+		pthread_mutex_lock(&philo->env->print);
+		ft_put_msg(philo, EVENT_FORK);
+		pthread_mutex_unlock(&philo->env->print);
 		pthread_mutex_lock(&philo->env->forks[philo->l_fork]);
-	}
-	pthread_mutex_lock(&philo->env->print);
-	ft_put_msg(philo, EVENT_FORK);
-	ft_put_msg(philo, EVENT_FORK);
-	pthread_mutex_unlock(&philo->env->print);
+		pthread_mutex_lock(&philo->env->print);
+		ft_put_msg(philo, EVENT_FORK);
+		pthread_mutex_unlock(&philo->env->print);
+	}	
 }
 
 void		ft_eat(t_philo *philo)
