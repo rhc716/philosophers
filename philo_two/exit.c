4db1f446ae2cf4c59e059635c0780ec0ca619ce1
melon <jack716@naver.com>
2021-03-20 23:11:39 +0900

    test

--- philo_two/exit.c
+++ philo_two/exit.c
@@ -6,7 +6,7 @@
 /*   By: hroh <hroh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 20:39:45 by hroh              #+#    #+#             */
-/*   Updated: 2021/03/19 21:16:35 by hroh             ###   ########.fr       */
+/*   Updated: 2021/03/20 23:10:31 by hroh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,6 @@ int		ft_clear(t_env *env)
 	sem_unlink(SEM_PRINT);
 	sem_unlink(SEM_END);
 	sem_unlink(SEM_FORKS);
-	sem_close(env->print_sem);
-	sem_close(env->end_sem);
-	sem_close(env->forks_sem);
 	free(env);
 	return (0);
 }
